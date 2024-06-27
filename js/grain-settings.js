4100cf319fb1bb8e6f4a71b06710be2531058d9a
joaopidranca <joaopidranca@gmail.com>
2024-06-27 23:49:49 +0100

    fixed grain

---
+++ js/grain-settings.js
@@ -0,0 +1,11 @@
+var options = {
+  animate: true,
+  patternWidth: 253.15,
+  patternHeight: 366.63,
+  grainOpacity: 0.07,
+  grainDensity: 1,
+  grainWidth: 1,
+  grainHeight: 1,
+};
+
+grained("#grainad", options);
