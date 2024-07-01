61910c94f31b8bce14d576d392493a3a1f698587
joaopidranca <joaopidranca@gmail.com>
2024-07-01 14:09:26 +0100

    footer fixes

--- js/grain-settings.js
+++ js/grain-settings.js
@@ -2,7 +2,7 @@ var options = {
   animate: true,
   patternWidth: 253.15,
   patternHeight: 366.63,
-  grainOpacity: 0.07,
+  grainOpacity: 0.04,
   grainDensity: 1,
   grainWidth: 1,
   grainHeight: 1,
