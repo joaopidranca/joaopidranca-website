b071d6069f3c1f6c4760a5d466997c7a4b674cc7
joaopidranca <joaopidranca@gmail.com>
2024-07-10 22:01:54 +0100

    javacript minified update

--- js/email-copy.js
+++
@@ -1,9 +0,0 @@
-//  onclick="copy(this)"
-function copy(that) {
-  var inp = document.createElement("input");
-  document.body.appendChild(inp);
-  inp.value = that.textContent;
-  inp.select();
-  document.execCommand("copy", false);
-  inp.remove();
-}
